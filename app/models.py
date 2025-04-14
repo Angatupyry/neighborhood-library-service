9e5293ed9a089943963b2fffe5feed1b067a18e3
Angatupyry <fierrofenix@gmail.com>
2025-04-14 08:00:00 -0300

    Prettier
    Signed-off-by: Angatupyry <fierrofenix@gmail.com>

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from tortoise import fields
 from tortoise.models import Model
 
+
 class BaseModel(Model):
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(null=True, auto_now=False)
 
     class Meta:
-        abstract = True  
+        abstract = True
+
 
 class Member(BaseModel):
     id = fields.IntField(pk=True)
@@ -16,12 +18,14 @@ class Member(BaseModel):
     phone_number = fields.CharField(max_length=20)
     address = fields.TextField()
 
+
 class Book(BaseModel):
     id = fields.IntField(pk=True)
     title = fields.CharField(max_length=255)
     author = fields.CharField(max_length=255)
     isbn = fields.CharField(max_length=13, unique=True)
     available = fields.BooleanField(default=True)
+
 
 class Borrow(BaseModel):
     id = fields.IntField(pk=True)
